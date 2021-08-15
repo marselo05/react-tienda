b5f4613b49924a75d9484022303862cd93430e17
Marcelo <marcelo.salar@gmail.com>
2021-08-14 22:40:34 -0300

    Tienda Deportiva / Salar Marcelo - Consumiendo APIs
    Detalle del producto A/B

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemCount from './components/ItemCount';
 
 
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <NavBar />
       <ItemListContainer />
+      <ItemDetailContainer />
       <ItemCount stock="5" initial="1" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
---
+++ src/components/ItemDetailContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ItemDetail from "./ItemDetail";
+
+class ItemDetailContainer extends React.Component
+{
+
+    render () 
+    {
+        return (
+            <ItemDetail />
+        )
+    }
+}
+
+export default ItemDetailContainer;
